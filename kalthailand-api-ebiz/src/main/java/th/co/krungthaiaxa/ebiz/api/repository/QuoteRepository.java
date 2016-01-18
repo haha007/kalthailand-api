aa083edd0a89729709327d1e87460a4662a1ae34
Cedric Arnoult <cedric.arnoult.ags@gmail.com>
2016-01-18 12:23:13 +0700

    Remove unnecessary dependency to MongoDB

--- kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/repository/QuoteRepository.java
+++ kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/repository/QuoteRepository.java
@@ -1,9 +1,10 @@
 package th.co.krungthaiaxa.ebiz.api.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import th.co.krungthaiaxa.ebiz.api.model.Quote;
 
 @Repository
-public interface QuoteRepository extends MongoRepository<Quote, String> {
+public interface QuoteRepository extends PagingAndSortingRepository<Quote, String> {
 }
--- kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/repository/SessionQuoteRepository.java
+++ kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/repository/SessionQuoteRepository.java
@@ -1,11 +1,12 @@
 package th.co.krungthaiaxa.ebiz.api.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 import th.co.krungthaiaxa.ebiz.api.model.enums.SessionType;
 import th.co.krungthaiaxa.ebiz.api.model.SessionQuote;
 
 @Repository
-public interface SessionQuoteRepository extends MongoRepository<SessionQuote, String> {
+public interface SessionQuoteRepository extends PagingAndSortingRepository<SessionQuote, String> {
     SessionQuote findBySessionIdAndSessionType(String sessionId, SessionType sessionType);
 }
