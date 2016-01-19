ad7b1ef6efb2d5f9c49338a3788ba759aab45867
Cedric Arnoult <cedric.arnoult.ags@gmail.com>
2016-01-19 11:16:31 +0700

    Add built-in support of jackson for java.time.LocalDate

--- kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/KalApiApplication.java
+++ kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/KalApiApplication.java
@@ -2,24 +2,11 @@ package th.co.krungthaiaxa.ebiz.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 @EnableSwagger2
 public class KalApiApplication {
-    @Bean
-    public Docket configureSwaggerDateAsString() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .build()
-                .directModelSubstitute(LocalDate.class, String.class);
-    }
-
     public static void main(String[] args) {
 		SpringApplication.run(KalApiApplication.class, args);
 	}
