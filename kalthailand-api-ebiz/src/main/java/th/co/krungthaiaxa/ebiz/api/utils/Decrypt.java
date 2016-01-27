4dacaf3be520a6788018c47f06d000e738915c82
Cedric Arnoult <cedric.arnoult.ags@gmail.com>
2016-01-27 17:54:47 +0700

    Introduce default key

--- kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/utils/Decrypt.java
+++ kalthailand-api-ebiz/src/main/java/th/co/krungthaiaxa/ebiz/api/utils/Decrypt.java
@@ -8,24 +8,30 @@ import javax.crypto.spec.SecretKeySpec;
 import java.net.URLDecoder;
 
 public class Decrypt {
+    private static final String DEFAULT_KEY = "defaultKey";
 
     public static String decrypt(String encrypted, String secretkey) throws Exception {
+        try {
+            return doDecrypt(encrypted, secretkey);
+        } catch (Exception e) {
+            return doDecrypt(encrypted, DEFAULT_KEY);
+        }
+    }
+
+    private static String doDecrypt(String encrypted, String secretkey) throws Exception {
         String utf8Encrypted = URLDecoder.decode(encrypted, "UTF-8");
         utf8Encrypted = StringUtils.replaceChars(utf8Encrypted, " ", "+");
         byte[] toDecode = Base64.decodeBase64(utf8Encrypted);
 
-        SecretKeySpec secretKey = new SecretKeySpec(hexToBytes(secretkey), "AES");
+        byte[] bytes = new byte[secretkey.getBytes("UTF-8").length / 2];
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) Integer.parseInt(secretkey.substring(i * 2, i * 2 + 2), 16);
+        }
+
+        SecretKeySpec secretKey = new SecretKeySpec(bytes, "AES");
         Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
         cipher.init(Cipher.DECRYPT_MODE, secretKey);
 
         return new String(cipher.doFinal(toDecode), "UTF-8");
-    }
-
-    private static byte[] hexToBytes(String hex) throws Exception {
-        byte[] bytes = new byte[hex.getBytes("UTF-8").length / 2];
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = (byte) Integer.parseInt(hex.substring(i * 2, i * 2 + 2), 16);
-        }
-        return bytes;
     }
 }
