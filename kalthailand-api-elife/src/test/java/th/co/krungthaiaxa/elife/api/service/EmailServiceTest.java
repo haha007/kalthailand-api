957370f91a6156bf3de9b71069e3736d8716a9d5
Cedric Arnoult <cedric.arnoult.ags@gmail.com>
2016-03-03 22:26:12 +0700

    Fix test failure

--- kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/elife/api/service/EmailServiceTest.java
+++ kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/elife/api/service/EmailServiceTest.java
@@ -123,9 +123,9 @@ public class EmailServiceTest {
         MimeMessage email = greenMail.getReceivedMessages()[0];
         assertThat(email.getSubject()).isEqualTo(subject);
         String bodyAsString = decodeSimpleBody(getBody(email));
-        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ขั้นต่ำ</td><td class=\"value\" align=\"right\" valign=\"top\" >2,968,718.00 บาท");
-        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับกลาง (รวมเงินปันผล**ระดับกลาง)</td><td class=\"value\" valign=\"top\" align=\"right\" >3,043,171.00 บาท");
-        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับสูง (รวมเงินปันผล**ระดับสูง)</td><td class=\"value\" valign=\"top\" align=\"right\" >3,062,746.00 บาท");
+        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ขั้นต่ำ</td><td class=\"value\" align=\"right\" valign=\"top\" >2,018,995.00 บาท");
+        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับกลาง (รวมเงินปันผล**ระดับกลาง)</td><td class=\"value\" valign=\"top\" align=\"right\" >2,253,818.00 บาท");
+        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับสูง (รวมเงินปันผล**ระดับสูง)</td><td class=\"value\" valign=\"top\" align=\"right\" >2,302,771.00 บาท");
     }
 
     @Test
@@ -140,9 +140,9 @@ public class EmailServiceTest {
         MimeMessage email = greenMail.getReceivedMessages()[0];
         assertThat(email.getSubject()).isEqualTo(subject);
         String bodyAsString = decodeSimpleBody(getBody(email));
-        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ขั้นต่ำ</td><td class=\"value\" align=\"right\" valign=\"top\" >1,484,359.00 บาท");
-        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับกลาง (รวมเงินปันผล**ระดับกลาง)</td><td class=\"value\" valign=\"top\" align=\"right\" >1,521,594.00 บาท");
-        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับสูง (รวมเงินปันผล**ระดับสูง)</td><td class=\"value\" valign=\"top\" align=\"right\" >1,531,376.00 บาท");
+        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ขั้นต่ำ</td><td class=\"value\" align=\"right\" valign=\"top\" >1,009,498.00 บาท");
+        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับกลาง (รวมเงินปันผล**ระดับกลาง)</td><td class=\"value\" valign=\"top\" align=\"right\" >1,126,910.00 บาท");
+        assertThat(bodyAsString).contains("รวมรับผลประโยชน์ระดับสูง (รวมเงินปันผล**ระดับสูง)</td><td class=\"value\" valign=\"top\" align=\"right\" >1,151,386.00 บาท");
     }
 
     @Test
