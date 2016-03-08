5e1645165bc0b0314691b4bacf5207682b9089f8
Cedric Arnoult <cedric.arnoult.ags@gmail.com>
2016-03-08 16:36:00 +0700

    Add API to upload Collection File

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/resource/RLSResource.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/resource/RLSResource.java
@@ -6,16 +6,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import th.co.krungthaiaxa.elife.api.data.CollectionFile;
 import th.co.krungthaiaxa.elife.api.service.RLSService;
 
 import javax.inject.Inject;
 
+import static org.springframework.http.HttpStatus.CREATED;
 import static org.springframework.http.HttpStatus.OK;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 import static th.co.krungthaiaxa.elife.api.utils.JsonUtil.getJson;
 
 @RestController
@@ -30,27 +34,18 @@ public class RLSResource {
         this.rlsService = rlsService;
     }
 
+    @ApiOperation(value = "Upload Collection files", notes = "Uploads a collection file and check for content validity", response = String.class)
+    @RequestMapping(value = "/RLS/collectionFile", produces = APPLICATION_JSON_VALUE, method = POST)
+    @ResponseBody
+    public ResponseEntity<byte[]> uploadCollectionFile(@RequestParam("file") MultipartFile file) {
+        return new ResponseEntity<>(getJson(""), CREATED);
+    }
+
     @ApiOperation(value = "Get Collection files", notes = "Get a list of all collection files", response = CollectionFile.class, responseContainer = "List")
     @RequestMapping(value = "/RLS/collectionFile", produces = APPLICATION_JSON_VALUE, method = GET)
     @ResponseBody
     public ResponseEntity getCollectionFiles() {
         logger.info("Getting all collection File");
-//        List<CollectionFile> result = new ArrayList<>();
-//
-//        CollectionFileLine collectionFileLine = new CollectionFileLine();
-//
-//        DeductionFile deductionFile = new DeductionFile();
-//        deductionFile.addLine(new DeductionFileLine());
-//
-//        CollectionFile collectionFile = new CollectionFile();
-//        collectionFile.setReceivedDate(LocalDateTime.now());
-//        collectionFile.setJobEndedDate(LocalDateTime.now());
-//        collectionFile.setJobStartedDate(LocalDateTime.now());
-//        collectionFile.setDeductionFile(deductionFile);
-//        collectionFile.addLine(collectionFileLine);
-//
-//        result.add(collectionFile);
-//        return new ResponseEntity<>(getJson(result), OK);
         return new ResponseEntity<>(getJson(rlsService.getCollectionFiles()), OK);
     }
 
