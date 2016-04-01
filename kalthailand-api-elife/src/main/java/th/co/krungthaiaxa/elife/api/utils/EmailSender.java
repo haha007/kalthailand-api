7cdc089d499cc1296335506a8a1597a5cec37695
Santi Likitmongkolsakool <SantiLik@krungthai-axa.co.th>
2016-04-01 13:05:29 +0700

    fixing wrong email quote encoding and change version to 1.1.14

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/utils/EmailSender.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/utils/EmailSender.java
@@ -57,9 +57,7 @@ public class EmailSender {
                 multipart.addBodyPart(getAttachmentBodyPart(attachment));
             }
         }
-        //multipart.addBodyPart(getContentBodyPart(emailContent, "text/html;charset=utf-8"));
-        //multipart.addBodyPart(getContentBodyPart(emailContent, "text/html;charset=ISO-8859-7"));
-        multipart.addBodyPart(getContentBodyPart(emailContent, "text/html;charset=TIS-620"));
+        multipart.addBodyPart(getContentBodyPart(emailContent, "text/html;charset=UTF-8"));
 
         // Get an SMTP Session Object based on the properties defined
         Session session = getSession(fromEmailAddress);
