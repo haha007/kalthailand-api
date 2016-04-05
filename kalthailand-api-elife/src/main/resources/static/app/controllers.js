1b8143dea9f5f51b689175b9b83cf1737c6cae7c
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-04-05 17:15:39 +0700

    Fix relative paths

--- kalthailand-api-elife/src/main/resources/static/app/controllers.js
+++ kalthailand-api-elife/src/main/resources/static/app/controllers.js
@@ -1,74 +1,84 @@
-(function() {
+(function () {
     'use strict';
 
     var app = angular.module('myApp');
 
-    app.controller('MainController', function($rootScope, $scope) {
+    app.controller('MainController', function ($rootScope, $scope) {
         $rootScope.accessErrorMessage = null;
     });
 
-    app.controller('AccessRightsDashboardController', function($rootScope, $scope, AccessRightsDashboard) {
+    app.controller('AccessRightsDashboardController', function ($rootScope, $scope, AccessRightsDashboard) {
         AccessRightsDashboard.get(
             {},
-            function(successResponse) {
+            function (successResponse) {
                 $rootScope.accessErrorMessage = null;
             },
-            function(errorResponse) {
+            function (errorResponse) {
                 $rootScope.accessErrorMessage = errorResponse.data.userMessage ? errorResponse.data.userMessage : errorResponse.data.message;
             })
     });
 
-    app.controller('AccessRightsAutopayController', function($rootScope, $scope, AccessRightsAutopay) {
+    app.controller('AccessRightsAutopayController', function ($rootScope, $scope, AccessRightsAutopay) {
         AccessRightsAutopay.get(
             {},
-            function(successResponse) {
+            function (successResponse) {
                 $rootScope.accessErrorMessage = null;
             },
-            function(errorResponse) {
+            function (errorResponse) {
                 $rootScope.accessErrorMessage = errorResponse.data.userMessage ? errorResponse.data.userMessage : errorResponse.data.message;
             })
     });
 
-    app.controller('AccessRightsValidationController', function($rootScope, $scope, AccessRightsValidation) {
+    app.controller('AccessRightsValidationController', function ($rootScope, $scope, AccessRightsValidation) {
         AccessRightsValidation.get(
             {},
-            function(successResponse) {
+            function (successResponse) {
                 $rootScope.accessErrorMessage = null;
             },
-            function(errorResponse) {
+            function (errorResponse) {
                 $rootScope.accessErrorMessage = errorResponse.data.userMessage ? errorResponse.data.userMessage : errorResponse.data.message;
             })
     });
 
-    app.controller('AppController', function($rootScope, $scope, Item) {
-        Item.query(function(response) {
+    app.controller('AppController', function ($rootScope, $scope, Item) {
+        Item.query(function (response) {
             $scope.collectionFiles = response;
         });
 
         $scope.file = null;
-        $scope.upload = function(event) {
+        $scope.upload = function (event) {
             event.preventDefault();
             var newItem = new Item;
             newItem.file = $scope.file;
 
             newItem.$save()
-                .then(function(resp) {
+                .then(function (resp) {
                     // For successfully state
-                    console.log(resp);
                     $scope.errorMessage = null;
                 })
-                .catch(function(resp) {
+                .catch(function (resp) {
                     // For error state
-                    console.log(resp);
-                    console.log(resp.data);
-                    console.log(resp.data.userMessage);
                     $scope.errorMessage = resp.data.userMessage;
                 });
         }
     });
 
-    app.controller('ValidationController', function($rootScope, $scope, PolicyDetail) {
-        $scope.search = function(event) {
+    app.controller('ValidationController', function ($rootScope, $scope, $http, PolicyDetail) {
+        $scope.onClickValidate = function (policyNumber) {
+            $http({
+                url: '/policies/' + policyNumber + '/update/status/validated',
+                method: 'PUT'
+            })
+                .then(
+                    function (successResponse) {
+                        alert("Policy has been validated");
+                    },
+                    function (errorResponse) {
+                        alert("Policy has NOT been validated");
+                    });
+        };
+
+        $scope.search = function (event) {
             event.preventDefault();
             PolicyDetail.get({id: $scope.policyID},
                 function (successResponse) {
@@ -86,12 +96,12 @@
                     if (periodicity == 'EVERY_MONTH') {
                         $scope.annualPremium = (premium * 12) + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
                     } else if (periodicity == 'EVERY_QUARTER') {
-                        $scope.annualPremium =  (premium * 4) + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
+                        $scope.annualPremium = (premium * 4) + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
                     } else if (periodicity == 'EVERY_HALF_YEAR') {
-                        $scope.annualPremium =  (premium * 2) + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
+                        $scope.annualPremium = (premium * 2) + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
                     }
                     else {
-                        $scope.annualPremium =  premium + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
+                        $scope.annualPremium = premium + " " + successResponse.premiumsData.financialScheduler.modalAmount.currencyCode;
                     }
                 },
                 function (errorResponse) {
