079e65a8f8563974c62cb69e72bb4c06060df71c
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-04-11 13:13:08 +0700

    Change parameters order to call Line Notification URL

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/service/PolicyService.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/service/PolicyService.java
@@ -223,7 +223,7 @@ public class PolicyService {
         // Send push notification
         try {
             String pushContent = IOUtils.toString(this.getClass().getResourceAsStream("/pushnotification-content/policy-purchased-notification.txt"), Charset.forName("UTF-8"));
-            lineService.sendPushNotification(policy.getInsureds().get(0).getPerson().getLineId(), pushContent.replace("%POLICY_ID%", policy.getPolicyId()));
+            lineService.sendPushNotification(pushContent.replace("%POLICY_ID%", policy.getPolicyId()), policy.getInsureds().get(0).getPerson().getLineId());
         } catch (IOException e) {
             logger.error(String.format("Unable to send push notification with policy id is [%1$s].", policy.getPolicyId()), e);
         }
