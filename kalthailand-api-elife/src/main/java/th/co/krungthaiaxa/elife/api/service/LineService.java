5784eeaf7a388ed672ee6360099150f60bf96716
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-04-23 19:00:22 +0200

    Fix Line Payment capture URL

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/service/LineService.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/elife/api/service/LineService.java
@@ -218,7 +218,7 @@ public class LineService {
 
         HttpEntity<String> entity = new HttpEntity<>(new String(JsonUtil.getJson(linePayConfirmingRequest)), headers);
 
-        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(linePayUrl + "/" + transactionId + "/capture");
+        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(linePayUrl + "/authorizations/" + transactionId + "/capture");
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response;
         try {
