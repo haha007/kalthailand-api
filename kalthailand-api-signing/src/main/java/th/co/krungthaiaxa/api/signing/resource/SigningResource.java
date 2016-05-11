eb54503ecc57d52a2b024d58defc7def8fddc155
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-05-11 15:34:22 +0700

    Remove refactored Classes

--- kalthailand-api-signing/src/main/java/th/co/krungthaiaxa/api/signing/resource/SigningResource.java
+++ kalthailand-api-signing/src/main/java/th/co/krungthaiaxa/api/signing/resource/SigningResource.java
@@ -1,26 +1,12 @@
 package th.co.krungthaiaxa.api.signing.resource;
 
-import io.swagger.annotations.*;
+import io.swagger.annotations.Api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 import th.co.krungthaiaxa.api.signing.service.SigningService;
-import th.co.krungthaiaxa.elife.api.model.LineBC;
-import th.co.krungthaiaxa.elife.api.model.Mid;
-import th.co.krungthaiaxa.elife.api.service.LineBCService;
-import th.co.krungthaiaxa.elife.api.utils.Decrypt;
 
 import javax.inject.Inject;
-import java.util.Optional;
-
-import static org.springframework.http.HttpStatus.BAD_REQUEST;
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static th.co.krungthaiaxa.elife.api.model.error.ErrorCode.*;
-import static th.co.krungthaiaxa.elife.api.utils.JsonUtil.getJson;
 
 @RestController
 @Api(value = "Document Signing")
