077f20ec8ada7cf56a3b541acc203d0e3017504c
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-05-25 13:06:37 +0700

    Mock all TMC calls to return success

--- kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/ELifeTest.java
+++ kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/ELifeTest.java
@@ -1,17 +1,23 @@
 package th.co.krungthaiaxa.api.elife;
 
 import org.junit.Before;
-import org.mockito.invocation.InvocationOnMock;
-import org.mockito.stubbing.Answer;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
 import th.co.krungthaiaxa.api.elife.client.AuthClient;
 import th.co.krungthaiaxa.api.elife.client.SigningClient;
 import th.co.krungthaiaxa.api.elife.repository.CDBRepository;
 import th.co.krungthaiaxa.api.elife.service.PolicyService;
+import th.co.krungthaiaxa.api.elife.tmc.TMCClient;
+import th.co.krungthaiaxa.api.elife.tmc.TMCSendingPDFResponse;
+import th.co.krungthaiaxa.api.elife.tmc.TMCSendingPDFResponseRemark;
+import th.co.krungthaiaxa.api.elife.tmc.wsdl.ReceivePDFJSON;
+import th.co.krungthaiaxa.api.elife.tmc.wsdl.ReceivePDFJSONResponse;
+import th.co.krungthaiaxa.api.elife.utils.JsonUtil;
 
 import javax.inject.Inject;
 import java.nio.charset.Charset;
@@ -29,35 +35,46 @@ public class ELifeTest {
     @Inject
     private AuthClient authClient;
     @Inject
+    private TMCClient tmcClient;
+    @Inject
     private SigningClient signingClient;
 
-    private CDBRepository cdbRepository;
-    private RestTemplate fakeSigningRestTemplate;
-    private RestTemplate fakeAuthRestTemplate;
     @Before
     public void setupFakeTemplateAndRepository() {
         // Faking signing by returning pdf document as received and 200 response
-        fakeSigningRestTemplate = mock(RestTemplate.class);
+        RestTemplate fakeSigningRestTemplate = mock(RestTemplate.class);
         signingClient.setTemplate(fakeSigningRestTemplate);
-        when(fakeSigningRestTemplate.exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class))).thenAnswer(new Answer<ResponseEntity<String>>() {
-            @Override
-            public ResponseEntity<String> answer(InvocationOnMock invocation) throws Throwable {
-                Object[] args = invocation.getArguments();
-                HttpEntity entity = (HttpEntity) args[2];
+        when(fakeSigningRestTemplate.exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class))).thenAnswer(invocation -> {
+            Object[] args = invocation.getArguments();
+            HttpEntity entity = (HttpEntity) args[2];
 
-                return new ResponseEntity<>(new String((byte[]) entity.getBody(), Charset.forName("UTF-8")), OK);
-            }
+            return new ResponseEntity<>(new String((byte[]) entity.getBody(), Charset.forName("UTF-8")), OK);
         });
 
         // Faking authorization by always returning success
-        fakeAuthRestTemplate = mock(RestTemplate.class);
+        RestTemplate fakeAuthRestTemplate = mock(RestTemplate.class);
         authClient.setTemplate(fakeAuthRestTemplate);
         when(fakeAuthRestTemplate.exchange(anyString(), any(HttpMethod.class), any(HttpEntity.class), eq(String.class))).thenReturn(getFakeToken());
 
         // Faking CDB by always returning empty Optional
-        cdbRepository = mock(CDBRepository.class);
+        CDBRepository cdbRepository = mock(CDBRepository.class);
         policyService.setCdbRepository(cdbRepository);
         when(cdbRepository.getExistingAgentCode(anyString(), anyString())).thenReturn(empty());
+
+        // Faking TMC by always returning success
+        WebServiceTemplate webServiceTemplate = mock(WebServiceTemplate.class);
+        tmcClient.setWebServiceTemplate(webServiceTemplate);
+        when(webServiceTemplate.marshalSendAndReceive(anyString(), any(ReceivePDFJSON.class), any(SoapActionCallback.class))).thenAnswer(invocation -> {
+            TMCSendingPDFResponseRemark tmcSendingPDFResponseRemark = new TMCSendingPDFResponseRemark();
+            tmcSendingPDFResponseRemark.setMessage("Success");
+
+            TMCSendingPDFResponse tmcSendingPDFResponse = new TMCSendingPDFResponse();
+            tmcSendingPDFResponse.setRemark(tmcSendingPDFResponseRemark);
+
+            ReceivePDFJSONResponse response = new ReceivePDFJSONResponse();
+            response.setReceivePDFJSONResult(new String(JsonUtil.getJson(tmcSendingPDFResponse)));
+            return response;
+        });
     }
 
     private ResponseEntity<String> getFakeToken() {
