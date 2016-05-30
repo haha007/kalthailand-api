371b42a6a5d60cc981bd083a54284a435795a00f
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-05-30 10:27:05 +0700

    Add default error handler

---
+++ kalthailand-api-blacklist/src/main/java/th/co/krungthaiaxa/api/blacklist/resource/ErrorResource.java
@@ -0,0 +1,36 @@
+package th.co.krungthaiaxa.api.blacklist.resource;
+
+import org.springframework.boot.autoconfigure.web.ErrorAttributes;
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+public class ErrorResource implements ErrorController {
+    private final ErrorAttributes errorAttributes;
+
+    @Inject
+    public ErrorResource(ErrorAttributes errorAttributes) {
+        Assert.notNull(errorAttributes, "ErrorAttributes must not be null");
+        this.errorAttributes = errorAttributes;
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+
+    @RequestMapping(value = "/error")
+    public Map<String, Object> error(HttpServletRequest aRequest) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(aRequest);
+        return errorAttributes.getErrorAttributes(requestAttributes, false);
+    }
+
+}
