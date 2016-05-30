a2d07d5023ba2f722840a658c3cd2a51046b6933
Cedric Arnoult <cedric.arnoult@gmail.com>
2016-05-30 18:51:28 +0700

    Remove unused method

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/repository/SessionQuoteRepository.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/repository/SessionQuoteRepository.java
@@ -2,13 +2,10 @@ package th.co.krungthaiaxa.api.elife.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-import th.co.krungthaiaxa.api.elife.model.Quote;
-import th.co.krungthaiaxa.api.elife.model.enums.ChannelType;
 import th.co.krungthaiaxa.api.elife.data.SessionQuote;
+import th.co.krungthaiaxa.api.elife.model.enums.ChannelType;
 
 @Repository
 public interface SessionQuoteRepository extends PagingAndSortingRepository<SessionQuote, String> {
     SessionQuote findBySessionIdAndChannelType(String sessionId, ChannelType channelType);
-
-    SessionQuote findByQuotesContaining(Quote quote);
 }
