41a61368ed29f3e959a640fb3ace0fc6a52cf134
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-08-02 08:25:11 +0700

    Add some log info for PolicyNumbersQuotaNotificationService.

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/policyNumbersQuota/service/PolicyNumbersQuotaNotificationService.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/policyNumbersQuota/service/PolicyNumbersQuotaNotificationService.java
@@ -95,7 +95,11 @@ public class PolicyNumbersQuotaNotificationService {
         if (lastNotificationTime == null) {
             return true;
         } else {
-            secondsFromLastNotification = Instant.now().getEpochSecond() - lastNotificationTime.getEpochSecond();
+            Instant now = Instant.now();
+            secondsFromLastNotification = now.getEpochSecond() - lastNotificationTime.getEpochSecond();
+            LOGGER.debug("Last notification time: {}, now: {}", lastNotificationTime.getEpochSecond(), now);
+            LOGGER.debug("SecondsFromLastNotification: {}", secondsFromLastNotification);
+            LOGGER.debug("NotificationTriggerSeconds: {}", notificationTriggerSeconds);
             return (secondsFromLastNotification + 1l) * timeScale >= notificationTriggerSeconds;
         }
     }
