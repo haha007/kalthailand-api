94639773f3e0dc437239e619b491cd7b5120a84a
Santi Likitmongkolsakool <SantiLik@krungthai-axa.co.th>
2016-09-29 11:28:24 +0700

    just commit

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/products/igen/IGenPdfRenderService.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/products/igen/IGenPdfRenderService.java
@@ -13,11 +13,29 @@ import com.itextpdf.text.PageSize;
 import com.itextpdf.text.pdf.PdfPTable;
 import com.itextpdf.text.pdf.PdfWriter;
 
+import th.co.krungthaiaxa.api.elife.model.Insured;
+import th.co.krungthaiaxa.api.elife.model.ProductIGenPremium;
 import th.co.krungthaiaxa.api.elife.model.Quote;
 import th.co.krungthaiaxa.api.elife.products.AbstractProductPdfRenderService;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
 @Service
 public class IGenPdfRenderService extends AbstractProductPdfRenderService {
+	
+	public static void main(String args[]){
+		List<String> list = new ArrayList<>();
+		list.add("a");
+		list.add("b");
+		list.add("c");
+		list.add("o");
+		System.out.println(list.stream().reduce((first, second) -> second).get());
+	}
+
 	
 	/*
 	 * must be implement for sale illustration pdf file generated
@@ -34,12 +52,17 @@ public class IGenPdfRenderService extends AbstractProductPdfRenderService {
 		Integer tbCols = 2;
         PdfPTable table1 = new PdfPTable(tbCols);
         
+        Insured insured = quote.getInsureds().stream().reduce((first, second) -> second).get();
+        ProductIGenPremium premium = quote.getPremiumsData().getProductIGenPremium();
+        
         String productWording = "แบบประกัน" + 
         messageSource.getMessage("product.id."+quote.getCommonData().getProductId(), null, thLocale) +
         " (" + messageSource.getMessage("product.id."+quote.getCommonData().getProductId(), null, null) +")";
         
         String dueWording = "งวดชำระ" +
         messageSource.getMessage("payment.mode."+quote.getPremiumsData().getFinancialScheduler().getPeriodicity().getCode().toString(), null, thLocale);
+        
+        Integer taxDeclared = (insured.getDeclaredTaxPercentAtSubscription()==null?0:insured.getDeclaredTaxPercentAtSubscription());
         
         table1.addCell(addData(productWording, getFontHeaderStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addData("เบี้ยประกัน", getFontHeaderStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
@@ -52,35 +75,35 @@ public class IGenPdfRenderService extends AbstractProductPdfRenderService {
         table1.addCell(addData("และเงินครบกำหนดสัญญา", getFontHeaderStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("กรณีรับครั้งเดียวโดยสะสมไว้กับ", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData("ขั้นต่ำ " + toCurrency(quote.getPremiumsData().getProductIGenPremium().getEndOfContractBenefit().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData("ขั้นต่ำ " + toCurrency(premium.getYearlyCashBacksForEndOfContract().stream().reduce((first, second) -> second).get().getAmount().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addData("บริษัทฯ ด้วยอัตราดอกเบี้ยขั้นต่ำ", getFontNormalStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addData("2% ต่อปี รับรวม", getFontNormalStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("กรณีเลือกรับทุกปี รับรวม", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(quote.getPremiumsData().getProductIGenPremium()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getYearlyCashBacksForAnnual().stream().reduce((first, second) -> second).get().getAmount().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("ผลประโยชน์กรณีเสียชีวิต", getFontHeaderStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("ปีที่ 1-3", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(0.0) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getYearlyDeathBenefits().get(0).getAmount().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addData("ปีที่ 4", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(0.0) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getYearlyDeathBenefits().get(3).getAmount().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addData("ปีที่ 5", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(0.0) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getYearlyDeathBenefits().get(4).getAmount().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addData("ปีที่ 6-10", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(0.0) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getYearlyDeathBenefits().get(5).getAmount().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("ผลประโยชน์การลดหย่อนภาษี", getFontHeaderStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("ลดหย่อนภาษีที่อัตราสูงสุด", getFontNormalGrayStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData("ที่ชำระ DUMMY% ต่อปี", getFontExtraSmallGrayStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
+        table1.addCell(addData("ที่ชำระ "+taxDeclared+"% ต่อปี", getFontExtraSmallGrayStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("ท่านจะได้รับผลประโยชน์จาก", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(0.0) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getYearlyTaxDeduction().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addData("การลดหย่อนภาษี ปีละ", getFontNormalStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
         table1.addCell(addData("รวมผลประโยชน์ทางภาษีจาก", getFontNormalStyle(), null, TB_HORIZONTAL_ALIGN_LEFT, null));
-        table1.addCell(addData(toCurrency(0.0) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
+        table1.addCell(addData(toCurrency(premium.getTotalTaxDeduction().getValue()) + " บาท", getFontNormalBlueStyle(), null, TB_HORIZONTAL_ALIGN_RIGHT, null));
         table1.addCell(addData("การชำระเบี้ยประกันภัย", getFontNormalStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addData("ตลอดสัญญา เป็นเงิน", getFontNormalStyle(), tbCols, TB_HORIZONTAL_ALIGN_LEFT, null));
         table1.addCell(addLine(false, tbCols));
--- kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/products/igen/IGenPdfRenderServiceTest.java
+++ kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/products/igen/IGenPdfRenderServiceTest.java
@@ -12,6 +12,8 @@ import static th.co.krungthaiaxa.api.elife.products.ProductType.PRODUCT_IFINE;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -28,7 +30,9 @@ import com.itextpdf.text.DocumentException;
 
 import th.co.krungthaiaxa.api.elife.KalApiApplication;
 import th.co.krungthaiaxa.api.elife.model.Amount;
+import th.co.krungthaiaxa.api.elife.model.DateTimeAmount;
 import th.co.krungthaiaxa.api.elife.model.Periodicity;
+import th.co.krungthaiaxa.api.elife.model.ProductIGenPremium;
 import th.co.krungthaiaxa.api.elife.model.Quote;
 import th.co.krungthaiaxa.api.elife.model.enums.GenderCode;
 import th.co.krungthaiaxa.api.elife.model.enums.PeriodicityCode;
@@ -53,12 +57,63 @@ public class IGenPdfRenderServiceTest {
 		ProductQuotation productQuotation = productQuotation(ProductType.PRODUCT_IGEN, 30, PeriodicityCode.EVERY_MONTH, 10000.0, true, 5, GenderCode.MALE);
 		Quote quote = quoteService.createQuote(randomNumeric(20), LINE, productQuotation);
         quote(quote, beneficiary(100.0));
+        quote.getPremiumsData().getFinancialScheduler().getPeriodicity().setCode(PeriodicityCode.EVERY_MONTH);
+        Amount amount = new Amount();
+        amount.setValue(10000.0);
+        amount.setCurrencyCode("THB");
+        quote.getPremiumsData().getFinancialScheduler().setModalAmount(amount);
+        ProductIGenPremium premium = new ProductIGenPremium();
+        List<DateTimeAmount> yearlyCashBacksForEndOfContract = new ArrayList<>();
+        for(int a=0;a<10;a++){
+        	DateTimeAmount dAmount = new DateTimeAmount();
+        	amount = new Amount();
+        	amount.setCurrencyCode("THB");
+        	amount.setValue(10000.0 * a);
+        	dAmount.setAmount(amount);
+        	yearlyCashBacksForEndOfContract.add(dAmount);
+        }
+        premium.setYearlyCashBacksForEndOfContract(yearlyCashBacksForEndOfContract);
+        List<DateTimeAmount> yearlyCashBacksForAnnual = new ArrayList<>();
+        for(int a=0;a<10;a++){
+        	DateTimeAmount dAmount = new DateTimeAmount();
+        	amount = new Amount();
+        	amount.setCurrencyCode("THB");
+        	amount.setValue(1000.0 * a);
+        	dAmount.setAmount(amount);
+        	yearlyCashBacksForAnnual.add(dAmount);
+        }
+        premium.setYearlyCashBacksForAnnual(yearlyCashBacksForAnnual);
+        List<DateTimeAmount> yearlyDeathBenefit = new ArrayList<>();
+        for(int a=0;a<10;a++){
+        	DateTimeAmount dAmount = new DateTimeAmount();
+        	amount = new Amount();
+        	amount.setCurrencyCode("THB");
+        	amount.setValue(1000.0 * a);
+        	dAmount.setAmount(amount);
+        	yearlyDeathBenefit.add(dAmount);
+        }
+        premium.setYearlyDeathBenefits(yearlyDeathBenefit);
+        quote.getInsureds().get(0).setDeclaredTaxPercentAtSubscription(20);
+        amount = new Amount();
+        amount.setCurrencyCode("THB");
+        amount.setValue(500.0);
+        premium.setYearlyTaxDeduction(amount);
+        amount = new Amount();
+        amount.setCurrencyCode("THB");
+        amount.setValue(1000.0);
+        premium.setTotalTaxDeduction(amount);
+        quote.getPremiumsData().setProductIGenPremium(premium);
+        
 
         Pair<byte[], String> pair = iGenPdfRenderService.generateSaleIllustrationPDF(quote);
         assertThat(pair.getLeft()).isNotEmpty();
         assertThat(pair.getRight()).isNotEmpty();
         FileUtils.writeByteArrayToFile(new File("target/" + pair.getRight()), pair.getLeft());
     }
+	
+	private Quote getIGenQuoteData(){
+		
+	}
 
 
 }
