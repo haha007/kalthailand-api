0817b5eb5f7021fb0e4f8ce7b189d39e8dba13e6
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-10-06 11:17:05 +0700

    Add default profiles for APIs

--- kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/ServletInitializer.java
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/ServletInitializer.java
@@ -1,13 +1,26 @@
 package th.co.krungthaiaxa.api.elife;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ServletInitializer.class);
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(KalApiElifeApplication.class);
+        return application.profiles(this.getDefaultProfile()).sources(KalApiElifeApplication.class);
     }
 
+    private String getDefaultProfile() {
+        String profile = System.getProperty("spring.profiles.active");
+        if (profile != null) {
+            LOGGER.info("Running with Spring profile(s) : {}", profile);
+            return profile;
+        }
+
+        LOGGER.warn("No Spring profile configured, running with default configuration");
+        return "dev";
+    }
 }
