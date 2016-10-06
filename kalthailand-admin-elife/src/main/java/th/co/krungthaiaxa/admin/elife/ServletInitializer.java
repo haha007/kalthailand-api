d2fdca555478b59f96ae9ebbfe03736b90a2d1ed
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-10-06 13:12:51 +0700

    Refactor to add default profile to APIs.

--- kalthailand-admin-elife/src/main/java/th/co/krungthaiaxa/admin/elife/ServletInitializer.java
+++ kalthailand-admin-elife/src/main/java/th/co/krungthaiaxa/admin/elife/ServletInitializer.java
@@ -4,23 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import th.co.krungthaiaxa.api.common.utils.ProjectProfileUtil;
 
 public class ServletInitializer extends SpringBootServletInitializer {
     private static final Logger LOGGER = LoggerFactory.getLogger(ServletInitializer.class);
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.profiles(this.getDefaultProfile()).sources(KalAdminApplication.class);
-    }
-
-    private String getDefaultProfile() {
-        String profile = System.getProperty("spring.profiles.active");
-        if (profile != null) {
-            LOGGER.info("Running with Spring profile(s) : {}", profile);
-            return profile;
-        }
-
-        LOGGER.warn("No Spring profile configured, running with default configuration");
-        return "dev";
+        return application.profiles(ProjectProfileUtil.getDefaultProfile()).sources(KalAdminApplication.class);
     }
 }
