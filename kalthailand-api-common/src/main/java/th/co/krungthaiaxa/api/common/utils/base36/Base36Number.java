03532ff8eb4a63b0df8e9daaa92a1e076a9c6862
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-10-28 15:11:10 +0700

    Create the base64Number

---
+++ kalthailand-api-common/src/main/java/th/co/krungthaiaxa/api/common/utils/base36/Base36Number.java
@@ -0,0 +1,99 @@
+package th.co.krungthaiaxa.api.common.utils.base36;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+/**
+ * @author khoi.tran on 10/28/16.
+ *         This system uses value from 0 -> 9, then A, B, C,... Z
+ *         10 decimal = A base36
+ *         16 decimal = F base36
+ *         35 decimal = Z base36
+ *         <p>
+ *         Note: There's no support for MongoDB convert or Json convert for this class yet. So please careful when using this class.
+ */
+public class Base36Number extends Number implements Comparable, Cloneable {
+    public static final int RADIX = 36;
+    public static final String MAX_BASE36_VALUE = "1y2p0ij32e8e7";
+    public static final String MIN_BASE36_VALUE = "-1y2p0ij32e8e8";
+    public static final Base36Number MAX_VALUE = new Base36Number(MAX_BASE36_VALUE);
+    public static final Base36Number MIN_VALUE = new Base36Number(MIN_BASE36_VALUE);
+
+    @NotNull
+    private final String base36Value;
+    private final long decimalValue;
+
+    public Base36Number() {
+        this.base36Value = "0";
+        this.decimalValue = Base36Util.toDecimalLong(base36Value);
+    }
+
+    public Base36Number(@NotNull String base36Value) {
+        this.base36Value = base36Value.toLowerCase();
+        this.decimalValue = Base36Util.toDecimalLong(base36Value);
+
+    }
+
+    public Base36Number(long decimalValue) {
+        this.base36Value = Base36Util.toBase36String(decimalValue);
+        this.decimalValue = decimalValue;
+    }
+
+    public String toString() {
+        return this.base36Value;
+    }
+
+    @Override
+    public Base36Number clone() {
+        return new Base36Number(this.decimalValue);
+    }
+
+    @Override
+    public int intValue() {
+        return (int) decimalValue;
+    }
+
+    @Override
+    public long longValue() {
+        return decimalValue;
+    }
+
+    @Override
+    public float floatValue() {
+        return (float) decimalValue;
+    }
+
+    @Override
+    public double doubleValue() {
+        return (double) decimalValue;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (o == null || !(o instanceof Base36Number)) {
+            return 1;
+        }
+        Base36Number number = (Base36Number) o;
+        long thisLong = this.longValue();
+        long thatLong = number.longValue();
+        if (thisLong > thatLong) {
+            return 1;
+        } else if (thisLong < thatLong) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return (this.compareTo(o) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        //The equals is evaluated based on the field decimalValue only, so this method must be also based on that field only.
+        return Objects.hash(this.decimalValue);
+    }
+
+}
---
+++ kalthailand-api-common/src/test/java/th/co/krungthaiaxa/api/common/utils/Base36NumberTest.java
@@ -0,0 +1,48 @@
+package th.co.krungthaiaxa.api.common.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import th.co.krungthaiaxa.api.common.utils.base36.Base36Number;
+import th.co.krungthaiaxa.api.common.utils.base36.Base36Util;
+
+/**
+ * @author khoi.tran on 10/28/16.
+ */
+public class Base36NumberTest {
+    public static final Logger LOGGER = LoggerFactory.getLogger(Base36NumberTest.class);
+
+    @Test
+    public void test_convert_decimal_to_base36() {
+        Assert.assertEquals(36, Base36Util.toDecimalLong("10"));
+        Assert.assertEquals(35, Base36Util.toDecimalLong("z"));
+        Assert.assertEquals(10, Base36Util.toDecimalLong("a"));
+        Assert.assertEquals(9, Base36Util.toDecimalLong("9"));
+        Assert.assertEquals(0, Base36Util.toDecimalLong("0"));
+    }
+
+    @Test
+    public void test_convert_base36_to_decimal() {
+        Assert.assertEquals("0", Base36Util.toBase36String(0));
+        Assert.assertEquals("9", Base36Util.toBase36String(9));
+        Assert.assertEquals("a", Base36Util.toBase36String(10));
+        Assert.assertEquals("h", Base36Util.toBase36String(17));
+        Assert.assertEquals("z", Base36Util.toBase36String(35));
+        Assert.assertEquals("10", Base36Util.toBase36String(36));
+
+        LOGGER.info("" + Base36Util.toBase36String(Long.MAX_VALUE));
+        LOGGER.info("" + Base36Util.toBase36String(Long.MIN_VALUE));
+
+    }
+
+    @Test
+    public void test_convert_max_value_of_base36l() {
+        Assert.assertEquals(Long.MAX_VALUE, Base36Util.toDecimalLong(Base36Number.MAX_BASE36_VALUE));
+        Assert.assertEquals(Long.MAX_VALUE, Base36Number.MAX_VALUE.longValue());
+
+        Assert.assertEquals(Long.MIN_VALUE, Base36Util.toDecimalLong(Base36Number.MIN_BASE36_VALUE));
+        Assert.assertEquals(Long.MIN_VALUE, Base36Number.MIN_VALUE.longValue());
+
+    }
+}
