dcc4eb2cd6584351eed3762c2bcbea89537be8e7
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-11-09 13:18:17 +0700

    Add UnitTest for other document types.

--- kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/test/service/DAFormServiceTest.java
+++ kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/test/service/DAFormServiceTest.java
@@ -1,6 +1,5 @@
 package th.co.krungthaiaxa.api.elife.test.service;
 
-import org.apache.commons.lang3.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,23 +9,19 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
-import th.co.krungthaiaxa.api.common.utils.DateTimeUtil;
-import th.co.krungthaiaxa.api.common.utils.IOUtil;
 import th.co.krungthaiaxa.api.elife.KalApiElifeApplication;
-import th.co.krungthaiaxa.api.elife.utils.TestUtil;
 import th.co.krungthaiaxa.api.elife.exception.QuoteCalculationException;
 import th.co.krungthaiaxa.api.elife.factory.PolicyFactory;
 import th.co.krungthaiaxa.api.elife.factory.productquotation.ProductQuotationFactory;
-import th.co.krungthaiaxa.api.elife.model.DocumentDownload;
 import th.co.krungthaiaxa.api.elife.model.Policy;
 import th.co.krungthaiaxa.api.elife.model.enums.AtpMode;
 import th.co.krungthaiaxa.api.elife.model.enums.DocumentType;
 import th.co.krungthaiaxa.api.elife.model.enums.PeriodicityCode;
 import th.co.krungthaiaxa.api.elife.products.ProductType;
-import th.co.krungthaiaxa.api.elife.products.utils.ProductUtils;
 import th.co.krungthaiaxa.api.elife.service.DocumentService;
 import th.co.krungthaiaxa.api.elife.service.PolicyService;
 import th.co.krungthaiaxa.api.elife.test.ELifeTest;
+import th.co.krungthaiaxa.api.elife.utils.DocumentAssertHelper;
 
 import javax.inject.Inject;
 
@@ -43,14 +38,19 @@ public class DAFormServiceTest extends ELifeTest {
     @Inject
     private DocumentService documentService;
     @Inject
+    private DocumentAssertHelper documentAssertHelper;
+    @Inject
     private PolicyFactory policyFactory;
 
     public void testWith(ProductType productType, PeriodicityCode periodicityCode, AtpMode atpMode) {
-        String msg = String.format("DAForm test: Product: %s, Periodicity: %s, ATP: %s", productType, periodicityCode, atpMode);
+        String msg = String.format("DAForm test: Product: %s, Periodicity: %s, ATP: %s.", productType, periodicityCode, atpMode);
         LOGGER.debug(msg);
         try {
             Policy policy = policyFactory.createPolicyWithPendingValidationStatus(ProductQuotationFactory.constructDefault(productType, periodicityCode, atpMode));
             assertMustHaveDAFormWhenAtpModeIsEnabled(policy, productType, periodicityCode, atpMode);
+            documentAssertHelper.assertHasDocument(policy, DocumentType.APPLICATION_FORM, msg + " Must contain ApplicationForm (not validated)");
+            documentAssertHelper.assertHasNoDocument(policy, DocumentType.APPLICATION_FORM_VALIDATED, msg + " Must not contain ApplicationForm (validated)");
+            documentAssertHelper.assertHasNoDocument(policy, DocumentType.ERECEIPT_PDF, msg + " Must not contain eReceiptPdf");
         } catch (Exception e) {
             if (PeriodicityCode.EVERY_MONTH.equals(periodicityCode) && AtpMode.NO_AUTOPAY.equals(atpMode)) {
                 Assert.assertTrue(e instanceof QuoteCalculationException);
@@ -59,15 +59,11 @@ public class DAFormServiceTest extends ELifeTest {
     }
 
     private void assertMustHaveDAFormWhenAtpModeIsEnabled(Policy policy, ProductType productType, PeriodicityCode periodicityCode, AtpMode atpMode) {
-        DocumentDownload documentDownload = documentService.findDocumentDownload(policy, DocumentType.DA_FORM);
+        String msg = String.format("DAForm test: Product: %s, Periodicity: %s, ATP: %s", productType, periodicityCode, atpMode);
         if (atpMode == AtpMode.AUTOPAY) {
-            Assert.assertNotNull(documentDownload);
-            byte[] pdfContent = documentService.getDocumentDownloadContent(documentDownload);
-            IOUtil.writeBytesToRelativeFile(TestUtil.PATH_TEST_RESULT + "/da-form/da-form_" + ProductUtils.validateExistProductName(policy) + DateTimeUtil.formatNowForFilePath() + ".pdf", pdfContent);
-            Assert.assertTrue(StringUtils.isNotBlank(documentDownload.getContent()));
+            documentAssertHelper.assertHasDocument(policy, DocumentType.DA_FORM, msg + " Must contain DAForm");
         } else {
-            String msg = String.format("DAForm test: Product: %s, Periodicity: %s, ATP: %s", productType, periodicityCode, atpMode);
-            Assert.assertNull(msg, documentDownload);
+            documentAssertHelper.assertHasNoDocument(policy, DocumentType.DA_FORM, msg + "Must not contain DAForm");
         }
     }
 
---
+++ kalthailand-api-elife/src/test/java/th/co/krungthaiaxa/api/elife/utils/DocumentAssertHelper.java
@@ -0,0 +1,36 @@
+package th.co.krungthaiaxa.api.elife.utils;
+
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Assert;
+import org.springframework.stereotype.Component;
+import th.co.krungthaiaxa.api.common.utils.DateTimeUtil;
+import th.co.krungthaiaxa.api.common.utils.IOUtil;
+import th.co.krungthaiaxa.api.elife.model.DocumentDownload;
+import th.co.krungthaiaxa.api.elife.model.Policy;
+import th.co.krungthaiaxa.api.elife.model.enums.DocumentType;
+import th.co.krungthaiaxa.api.elife.products.utils.ProductUtils;
+import th.co.krungthaiaxa.api.elife.service.DocumentService;
+
+import javax.inject.Inject;
+
+/**
+ * @author khoi.tran on 11/9/16.
+ */
+@Component
+public class DocumentAssertHelper {
+    @Inject
+    private DocumentService documentService;
+
+    public void assertHasDocument(Policy policy, DocumentType documentType, String msg) {
+        DocumentDownload documentDownload = documentService.findDocumentDownload(policy, documentType);
+        Assert.assertNotNull(msg, documentDownload);
+        byte[] pdfContent = documentService.getDocumentDownloadContent(documentDownload);
+        IOUtil.writeBytesToRelativeFile(TestUtil.PATH_TEST_RESULT + "/documentType/" + DateTimeUtil.formatNowForFilePath() + "_" + ProductUtils.validateExistProductName(policy) + ".pdf", pdfContent);
+        Assert.assertTrue(msg, StringUtils.isNotBlank(documentDownload.getContent()));
+    }
+
+    public void assertHasNoDocument(Policy policy, DocumentType documentType, String msg) {
+        DocumentDownload documentDownload = documentService.findDocumentDownload(policy, documentType);
+        Assert.assertNull(msg, documentDownload);
+    }
+}
