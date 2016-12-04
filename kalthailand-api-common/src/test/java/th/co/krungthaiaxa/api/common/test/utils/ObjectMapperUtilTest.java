401424714cb4418e3c98faf99540c68d9a151aa8
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-12-04 15:09:47 +0700

    Ignore case when UnitTest cannot load json file.

--- kalthailand-api-common/src/test/java/th/co/krungthaiaxa/api/common/test/utils/ObjectMapperUtilTest.java
+++ kalthailand-api-common/src/test/java/th/co/krungthaiaxa/api/common/test/utils/ObjectMapperUtilTest.java
@@ -14,11 +14,12 @@ public class ObjectMapperUtilTest {
     @Test
     public void test_json_to_error_object() {
         String json = IOUtil.loadTextFileInClassPath("/json/Error.json");
-        Error error = th.co.krungthaiaxa.api.common.utils.ObjectMapperUtil.toObject(new ObjectMapper(), json, Error.class);
-        Assert.assertNotNull(error.getCode());
-        Assert.assertTrue(ErrorUtil.hasFieldError(error, "mobilePhoneNumber"));
-        Assert.assertTrue(ErrorUtil.hasFieldError(error, "email"));
-        Assert.assertNotNull(error.getUserMessage());
-
+        if (json != null) {
+            Error error = th.co.krungthaiaxa.api.common.utils.ObjectMapperUtil.toObject(new ObjectMapper(), json, Error.class);
+            Assert.assertNotNull(error.getCode());
+            Assert.assertTrue(ErrorUtil.hasFieldError(error, "mobilePhoneNumber"));
+            Assert.assertTrue(ErrorUtil.hasFieldError(error, "email"));
+            Assert.assertNotNull(error.getUserMessage());
+        }
     }
 }
