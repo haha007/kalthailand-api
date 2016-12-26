bebc619a74c94bbbc2320bae5396c4c5745650cd
Khoi Tran Nguyen Minh <khoi.tran@pyramid-consulting.com>
2016-12-26 11:15:30 +0700

    v1.13.0: Add customer campaign for Krungthai-AXA 20th Anniversary.

---
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/customer/campaign/krungthaiaxa20th/CustomerCampaignResource.java
@@ -0,0 +1,11 @@
+package th.co.krungthaiaxa.api.elife.customer.campaign.krungthaiaxa20th;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author khoi.tran on 12/26/16.
+ */
+@RestController
+public class CustomerCampaignResource {
+    
+}
---
+++ kalthailand-api-elife/src/main/java/th/co/krungthaiaxa/api/elife/customer/campaign/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains business logic for customers to input their information. From those information, the company can do some upselling or give the customer some promotion buying.
+ * For example: AKT-1256: Create registration form campaign “Krungthai-AXA 20th Anniversary” for customer fill information.
+ *
+ * @author khoi.tran on 12/26/16.
+ */
+package th.co.krungthaiaxa.api.elife.customer.campaign;
